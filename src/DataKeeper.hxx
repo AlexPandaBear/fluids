f900de72f7712e0a58fc022b2de11dbf8a0c807c
AlexPandaBear <alexandre.dutka@student.isae-supaero.fr>
2020-09-30 01:24:13 +0200

    v2.0.3

--- src/DataKeeper.hxx
+++ src/DataKeeper.hxx
@@ -5,6 +5,9 @@
 #include <fstream>
 #include <iostream>
 
+/**
+ * A class responsible for the storage of the simulation data and the management of the simulation files
+ */
 class DataKeeper
 {
 private:
@@ -21,21 +24,146 @@ private:
 	std::unique_ptr<double[]> ptr_V;
 
 public:
+	/**
+	 * The constructor of the class
+	 */
 	DataKeeper();
+
+	/**
+	 * The destructor of the class
+	 */
 	~DataKeeper();
 
+	/**
+	 * A method reshaping the containers to fit the data computed by a new simulation
+	 * 
+	 * @param nb_steps The number of steps in the temporal integration
+	 *
+	 * @param nx The number of points of computation along the x-axis
+	 *
+	 * @param ny The number of points of computation along the y-axis
+	 *
+	 * @warning When calling this method, any data previously stored in this instance will be lost
+	 */
 	void reset_size(size_t nb_steps, size_t nx, size_t ny);
 
+	/**
+	 * A method accessing the temperature value stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 */
 	double getTemperatureAt(size_t t, size_t i, size_t j) const;
+
+	/**
+	 * A method accessing the pressure value stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 */
 	double getPressureAt(size_t t, size_t i, size_t j) const;
+
+	/**
+	 * A method accessing the value of the x-coordinate of the velocity stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 */
 	double getXVelocityAt(size_t t, size_t i, size_t j) const;
+
+	/**
+	 * A method accessing the value of the y-coordinate of the velocity stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 */
 	double getYVelocityAt(size_t t, size_t i, size_t j) const;
 
+	/**
+	 * A method setting the temperature value stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 *
+	 * @param T The value to store
+	 *
+	 * @warning Calling this method will overwrite the previously stored value
+	 */
 	void setTemperatureAt(size_t t, size_t i, size_t j, double T);
+
+	/**
+	 * A method setting the pressure value stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 *
+	 * @param P The value to store
+	 *
+	 * @warning Calling this method will overwrite the previously stored value
+	 */
 	void setPressureAt(size_t t, size_t i, size_t j, double P);
+
+	/**
+	 * A method setting the value of the x-coordinate of the velocity stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 *
+	 * @param U The value to store
+	 *
+	 * @warning Calling this method will overwrite the previously stored value
+	 */
 	void setXVelocityAt(size_t t, size_t i, size_t j, double U);
+
+	/**
+	 * A method setting the value of the y-coordinate of the velocity stored at a specific moment and location
+	 *
+	 * @param t The time index of the value
+	 *
+	 * @param i The x-coordinate index of the point
+	 *
+	 * @param j The y-coordinate index of the point
+	 *
+	 * @param V The value to store
+	 *
+	 * @warning Calling this method will overwrite the previously stored value
+	 */
 	void setYVelocityAt(size_t t, size_t i, size_t j, double V);
 
+	/**
+	 * A method saving all the current state in the instance in a file in order to get back to this state later
+	 *
+	 * @param file_name The full name (with the eventual path) of the file to create
+	 *
+	 * @warning If the name chosen matches an existing file, this method will overwrite the existing file
+	 */
 	void saveData(std::string file_name) const;
+
+	/**
+	 * A method loading all the data from a file in order to resume a previous state of an instance of the class
+	 *
+	 * @param file_name The full name (with the eventual path) of the file to load
+	 */
 	void loadData(std::string file_name);
 };
--- src/ThermalKernel.hxx
+++ src/ThermalKernel.hxx
@@ -5,6 +5,10 @@
 
 #include "DataKeeper.hxx"
 
+/**
+ * A class designed to be called by an instance of SimManager in order to solve the heat equation.
+ */
+
 class ThermalKernel
 {
 private:
@@ -43,8 +47,48 @@ private:
 	void printSimulationProgression(size_t t) const;
 
 public:
+	/**
+	 * The constructor of the class
+	 *
+	 * @param data The DataKeeper object in which the computed solution will be stored
+	 *
+	 * @param tMax The duration of the simulation
+	 *
+	 * @param nb_steps The number of steps to perform for the temporal integration
+	 *
+	 * @param theta The parameter of the theta-scheme used for the temporal integration
+	 *
+	 * @param accuracy The accuracy demanded for the computed temperatures (stop condition for the Gauss-Seidel algorithm)
+	 *
+	 * @param Lx The length along the x-axis of the domain to simulate
+	 *
+	 * @param Ly The length along the y-axis of the domain to simulate
+	 *
+	 * @param nx The number of points of computation along the x-axis
+	 *
+	 * @param ny The number of points of computation along the y-axis
+	 *
+	 * @param lambda The thermal conductivity of the fluid
+	 *
+	 * @param rho The volumic mass of the fluid
+	 *
+	 * @param cp The heat capacity per unit of mass of the fluid
+	 *
+	 * @param T_BC The temperature at the boundary of the domain
+	 */
 	ThermalKernel(DataKeeper& data, double tMax, size_t nb_steps, double theta, double accuracy, double Lx, double Ly, size_t nx, size_t ny, double lambda, double rho, double cp, double T_BC);
+	
+	/**
+	 * The destructor of the class
+	 */
 	~ThermalKernel();
 	
+	/**
+	 * A method solving the heat equation thanks to the parameters previously defined
+	 *
+	 * @warning To work properly, this method has to be called after the computation of the flow.
+	 *
+	 * @warning To work properly, this method has to be called after defining the initial temperature field in the DataKeeper object used to create the instance.
+	 */
 	void simulate();
 };
